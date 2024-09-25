782270e90920e9837655def90e4b1efc08692aa8
Your Name <yourname@example.com>
2024-09-25 14:09:33 +0530

    ADDED A RESET BUTTON
    AND MADE SUM VISIBLE IN THE UI

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ let hasBlackJack = false
 let isAlive = true
 let message = "";
 const btn = document.querySelector("#btn");
+const reset = document.querySelector("#reset");
 let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el");
 
 btn.addEventListener("click", function startGame() {
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 😀";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Woohoo! You've got Blackjack! 😍";
+        message = "You've got Blackjack!";
         hasBlackJack = true
     } else  {
-        message = "You're out of the game! 🥸";
+        message = "You're out of the game!";
         isAlive = false
     }
     messageEl.textContent = message;
+})
+
+reset.addEventListener("click", function resetGame() {
+    messageEl.textContent = "Want to play a round?";
+    sumEl.textContent = "Sum: ";
 })
 
