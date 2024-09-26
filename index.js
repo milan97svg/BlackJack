5642452ae1fe88a9c18f8c07fd41a89ea487dc43
Your Name <yourname@example.com>
2024-09-26 11:16:23 +0530

    NEW BUTTONS TO ADD A NEW CARD WHEN THE FIRST TWO CARDS ARE BELOW 21.
    CHANGED THE BUTTONS TO FLEX DISPLAY

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
+let cards = [firstCard, secondCard] // array - ordered list of cards
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = "";
 const btn = document.querySelector("#btn");
-const reset = document.querySelector("#reset");
-let messageEl = document.querySelector("#message-el")
+const reset = document.querySelector("#newgame");
+const cardEl = document.querySelector("#newcard")
+let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
-btn.addEventListener("click", function startGame() {
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -21,10 +29,22 @@ btn.addEventListener("click", function startGame() {
         isAlive = false
     }
     messageEl.textContent = message;
-})
+}
 
-reset.addEventListener("click", function resetGame() {
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 7;
+    sum += card;
+    startGame();
+}
+
+function newGame() {
     messageEl.textContent = "Want to play a round?";
     sumEl.textContent = "Sum: ";
-})
+    cardsEl.textContent = "Cards: ";
+}
+
+btn.addEventListener("click", startGame)
+cardEl.addEventListener("click", newCard)
+reset.addEventListener("click", newGame)
 
